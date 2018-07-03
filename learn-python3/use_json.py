f92aaa5d25e10541f9986d210f6f86e925a7cbb5
Youngdee <echonull@qq.com>
2018-07-03 15:34:17 +0800

    学习python序列化操作

--- learn-python3/use_json.py
+++ learn-python3/use_json.py
@@ -1,5 +1,16 @@
 import json
-json_str = '{"name":"youngdee", "age": 24}'
-student = json.loads(json_str)
-print(type(student))
-print(student)
+# 反序列化
+# json_str = '{"name":"youngdee", "age": 24}'
+# student = json.loads(json_str)
+# print(type(student))
+# print(student)
+
+
+# 序列化
+student = [
+    {'name': 'YoungDee', 'age': 24, 'flag': False},
+    {'name': 'Mona', 'age': 18}
+]
+json_str = json.dumps(student)
+print(type(json_str))
+print(json_str)
